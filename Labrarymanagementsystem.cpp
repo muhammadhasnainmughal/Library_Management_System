23f6d9ecefa41540d6a2edcf62b40d63fd016524
muhammadhasnainmughal <muhammadhasnain.bscsf23@iba-suk.edu.pk>
2023-12-11 22:57:59 +0500

    updated code
    only login system and interface add

--- Labrarymanagementsystem.cpp
+++ Labrarymanagementsystem.cpp
@@ -2,9 +2,39 @@
 using namespace std;
 int main()
 {
+	//interface
+	cout << "\t\t\t\t\t-------------------------------" << endl;
+	cout << "\t\t\t\t\t   Library Management System" << endl;
+	cout << "\t\t\t\t\t-------------------------------" << endl;
 	
-	cout << "Hello World";
- cout << "Hasnain";
+	cout << "-----------------" << endl ;
+	cout << "      Login!" << endl;
+	cout << "-----------------" << endl;
+	
+	//make login system
+	string username,password;
+	
+	cout << "Enter Username: ";
+	cin >> username;
+	
+	cout << "Enter Password: ";
+	cin >> password;
+
+	if(username == "admin" && password == "admin")
+	{
+		cout << "Login Successfully..." << endl;
+	}
+	else
+	{
+		cout << "Invalid Username or Password Tryagain" << endl;
+	}
+	
+        system("PAUSE");
+		system("CLS");
+	
+	
+	
 	
 	return 0;
 }
+
